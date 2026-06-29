b69e0fa0c7fba55246d97568f08d9a088a13b38f
mara <maramariano000@gmail.com>
2026-06-29 23:39:23 +0200

    Update PWA icons configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -24,20 +24,10 @@ export default defineConfig({
         orientation: 'portrait',
         icons: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
+            src: 'icon.png',
+            sizes: '512x512', 
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'any'
           }
         ]
       },
